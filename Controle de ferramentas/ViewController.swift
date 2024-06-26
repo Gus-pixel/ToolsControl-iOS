cbd4123ab3b3d30884ed622ecd89699602ce96a8
Gustavo Ramos <gustavo.ramos7049@gmail.com>
2024-06-26 20:34:03 -0300

    Initial Commit

---
+++ Controle de ferramentas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Controle de ferramentas
+//
+//  Created by Gustavo on 26/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
